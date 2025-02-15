dd7d2a9a857a84c5f1b88ce5177113eb7fdf463c
Егор Купрацевич <kupratsevich.egor@gmail.com>
2025-02-15 18:50:43 +0300

    searchBar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './components/footer/footer.tsx';
 import Header from './components/header/header.tsx';
+import SearchBar from './components/searchBar/searchBar.tsx';
 
 function App() {
     return (
         <>
             <Header />
+            <SearchBar placeholder="Search art, artist, work..."/>
             <Footer></Footer>
         </>
     );
--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,15 +1,23 @@
-// import { FC } from 'react';
-//
-//
-// interface searchBarProps {
-//     value: string;
-// }
-//
-// const searchBar: FC<searchBarProps> = (props) => {
-//
-//     return (
-//
-//     );
-// };
-//
-// export default searchBar;
+import { FC } from 'react';
+import styles from './searchBar.module.scss'
+import searchIcon from '@assets/searchicon.svg';
+
+
+interface searchBarProps {
+    placeholder: string;
+}
+
+const SearchBar: FC<searchBarProps> = (props) => {
+
+
+
+    return (
+        <div className={styles.searchContainer}>
+            <input placeholder={props.placeholder} className={styles.searchBar} />
+            <img className={styles.searchIcon} src={searchIcon} alt="searchIcon" width={32} height={32} />
+        </div>
+
+    );
+};
+
+export default SearchBar;
