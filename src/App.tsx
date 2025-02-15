ac31b7782bfa15328d262ebbd2fd42753e032830
Егор Купрацевич <kupratsevich.egor@gmail.com>
2025-02-15 18:14:38 +0300

    husky fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from './components/header/header.tsx';
 function App() {
     return (
         <>
-            <Header/>
+            <Header />
             <Footer></Footer>
         </>
     );
---
+++ src/components/searchBar/searchBar.tsx
@@ -0,0 +1,15 @@
+// import { FC } from 'react';
+//
+//
+// interface searchBarProps {
+//     value: string;
+// }
+//
+// const searchBar: FC<searchBarProps> = (props) => {
+//
+//     return (
+//
+//     );
+// };
+//
+// export default searchBar;
