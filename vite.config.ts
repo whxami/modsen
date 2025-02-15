6bb7a00b9daf666e6e20ee27065b922a68aa5e2b
Егор Купрацевич <kupratsevich.egor@gmail.com>
2025-02-15 17:36:49 +0300

    header added

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,21 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@styles/colors/colors.scss"; @import "@styles/fonts/fonts.scss";`,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@utils': path.resolve(__dirname, 'src/utils'),
       '@pages': path.resolve(__dirname, 'src/pages'),
-      '@public': path.resolve(__dirname, 'public'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@styles': path.resolve(__dirname, 'src/shared/styles'),
     },
   },
 });
